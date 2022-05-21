391a4bf85a352a845e798dd92f6e86f3d8a8f967
genelem <genesiselealmunoz@gmail.com>
2022-05-20 22:58:53 -0300

    React

---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
+import SearchMovies from './SearchMovies';
+import Footer from './Footer';
+
+function ContentWrapper(){
+    return (
+        <Reacciona. Fragmento>
+            {/*<!-- Contenedor de contenido -->*/}
+            <div id="content-wrapper" className="d-flex flex-column">
+                {/*<!-- Contenido principal -->*/}
+                <div id="contenido">
+                    <Barra superior />
+                    <ContentRowTop />
+                    < SearchMovies />
+                    <Pie de página />
+                </div>
+            </div>    
+        </Reacciona. Fragmento>
+    )
+}
+export default ContentWrapper;
